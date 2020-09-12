6148e3c2c6a46f2ba9e51ae209b3da454f508f3e
Lw中 <1544712020@qq.com>
2020-09-12 19:39:44 +0800

    完善

--- src/main/java/com/lwz/scaffold/controller/HelloController.java
+++ src/main/java/com/lwz/scaffold/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
@@ -24,4 +25,5 @@ public class HelloController {
     public String user() {
         return "user";
     }
+
 }
