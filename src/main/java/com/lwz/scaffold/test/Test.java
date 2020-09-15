672cb2bea35c07d86fc1926aab5be1ee45f8cb6e
Lw中 <1544712020@qq.com>
2020-09-15 19:14:00 +0800

    测试

---
+++ src/main/java/com/lwz/scaffold/test/Test.java
@@ -0,0 +1,13 @@
+package com.lwz.scaffold.test;
+
+/**
+ * @author Lw中
+ * @date 2020/9/15 19:13
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
