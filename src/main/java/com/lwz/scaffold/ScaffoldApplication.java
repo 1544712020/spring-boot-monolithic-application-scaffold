1bba544a313af18b3a3b13598a94566c9a3e0459
Lw中 <1544712020@qq.com>
2020-09-28 15:37:23 +0800

    完善Swagger整合

--- src/main/java/com/lwz/scaffold/ScaffoldApplication.java
+++ src/main/java/com/lwz/scaffold/ScaffoldApplication.java
@@ -1,13 +1,18 @@
 package com.lwz.scaffold;
 
+import com.lwz.scaffold.config.RedisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Lw中
  */
 @EnableCaching
+@Import(RedisConfig.class)
+@EnableSwagger2
 @SpringBootApplication
 public class ScaffoldApplication {
 
